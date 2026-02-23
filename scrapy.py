856d91104ce12614c4b296fc8492fbaeedf7ea70
PRAVEEN KUMAR <prava630@gmail.com>
2026-02-23 23:07:01 +0530

    Update scrapy.py

--- scrapy.py
+++ scrapy.py
@@ -11,36 +11,29 @@ try:
     headers ={
         "User-Agent":"Mozilla/5.0"
     }
-
     response=requests.get(url,headers=headers)
-
     if response.status_code==200:
         try:
             soup=BeautifulSoup(response.content,'html.parser')
-
             if soup.title:
                 print(soup.title.get_text().strip())
             else:
                 print()
-
             if soup.body:
                 print(soup.body.get_text().strip())
             else:
                 print()
-
             links=soup.find_all('a')
             for link in links:
-                url_link = link.get("href")
+                url_link=link.get("href")
                 if url_link:
                     print(url_link)
                 else:
                     print()
-
         except Exception as excep:
             print()
-
     else:
         print()
-
 except Exception as excep:
+
     print()
